7facf95da07b17dde301d0538c9800684753914b
Wiiitek <wiiitek@users.noreply.github.com>
2021-03-27 16:39:18 +0100

    update for unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('timeismoney app is running!');
+    expect(compiled.querySelector('h1.main').textContent).toContain('timeismoney');
   });
 });
