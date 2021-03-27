1bc3220ddbf8f1a23bfb8a67d5849bd4fdc45fb2
Wiiitek <wiiitek@users.noreply.github.com>
2021-03-27 13:48:51 +0100

    check added so that we always have 80% of coverage

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,15 @@ module.exports = function (config) {
         //{ type: 'html' },
         { type: 'text-summary' },
         { type: 'lcovonly' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
