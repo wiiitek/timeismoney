c49510373ab9750a0906069c2288c72313c2d42b
Wiiitek <wiiitek@users.noreply.github.com>
2021-03-28 13:21:51 +0200

    update for colors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1.main').textContent).toContain('timeismoney');
+    expect(compiled.querySelector('h1').textContent).toContain('timeismoney');
   });
 });
