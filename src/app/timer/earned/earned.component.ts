48d7d389b2f9bc670d396b1c78d07fe81342e893
Wiiitek <wiiitek@users.noreply.github.com>
2021-04-05 00:05:59 +0200

    formatting for time and earned money

--- src/app/timer/earned/earned.component.ts
+++ src/app/timer/earned/earned.component.ts
@@ -9,15 +9,29 @@ import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@a
 })
 export class EarnedComponent {
 
-  wholeNumber = 0;
+  wholeNumber = '000';
 
-  cents = 0;
+  cents = '00';
 
   @Input()
   set earned(earned: number | any) {
     const value = earned || 0;
-    this.cents = value % 100;
-    this.wholeNumber = Math.floor(value / 100)
+
+    const centsValue = (value % 100).toString();
+    this.cents = centsValue.length === 1 ? ('0' + centsValue) : centsValue;
+
+    const wholeValue = Math.floor(value / 100).toString();
+    const length = wholeValue.length;
+    switch (length) {
+      case 1: this.wholeNumber = '00' + wholeValue; break;
+      case 2: this.wholeNumber = '0' + wholeValue; break;
+      default: this.wholeNumber = wholeValue;
+    }
+
   }
 
+  private present(value: number): string {
+    const str = value.toString();
+    return str.length === 1 ? ('0' + str) : str;
+  }
 }
