09eccafd4e8090f8da77ec98a0b6bec82406e571
Wiiitek <wiiitek@users.noreply.github.com>
2021-04-04 22:02:20 +0200

    change dependency to private

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -29,7 +29,7 @@ export class TimerComponent {
     return this.timerService.counting ? countingCss : notCountingCss;
   }
 
-  constructor(public timerService: TimerService) { }
+  constructor(private timerService: TimerService) { }
 
   onStartOrPause() {
     this.timerService.onStartOrPause();
