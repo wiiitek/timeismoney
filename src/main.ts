e1773959a6ca9aff2b987837303a2d222f5345fe
Wiiitek <wiiitek@users.noreply.github.com>
2025-11-22 10:19:11 +0100

    Upgrade Angular from version 20 to 21 (#1226)
    * Add some missing modules
    
    * Upgrade made with ng tool
    
    * fix for issue with angular rarma plugin now being automatically loaded
    
    * Formatting changes
    
    * Remove not needed dependency
    
    * Revert version 0.16 for zone.js
    
    ---------
    
    Co-authored-by: Witek <wiiitek>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -12,6 +12,7 @@ if (environment.production) {
 
 bootstrapApplication(App, {
   providers: [
+    provideZoneChangeDetection(),
     importProvidersFrom(BrowserModule, FormsModule),
     RateService
   ]
