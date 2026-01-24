db054086eb74ff6e6e36f2e441cff15f703a58b8
Witek <wiiitek>
2026-01-24 15:08:42 +0100

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,4 @@
-// this file is copied from
-// https://angular.io/guide/testing#configuration
-// and required to configure coverage reporter
-// (defaults are HTML only report and we need something for SonarQube)
-module.exports = function (config) {
-  config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-junit-reporter"),
-      require("karma-coverage"),
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
-    junitReporter: {
-      outputDir: "test-results",
-      outputFile: "junit.xml",
-      useBrowserName: false,
-    },
-    coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/"),
-      subdir: ".",
-      // https://github.com/karma-runner/karma-coverage/blob/master/docs/configuration.md
-      reporters: [
-        { type: "html" },
-        { type: "text-summary" },
-        { type: "lcov" }
+// Karma config removed. Project now uses Vitest for testing (Angular 21+ default).
       ],
     },
     reporters: ["progress", "kjhtml", "coverage", "junit"],
