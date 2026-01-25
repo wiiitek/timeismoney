39150dd5a44437de86f809368302eb6532119cf7
Witek <wiiitek>
2026-01-25 08:10:15 +0100

    Make angular read vitest config
    Tests result in XML file
    
    Fixes for JUnit test report and coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,20 @@ export default defineConfig({
     globals: true,
     setupFiles: './src/test-setup.ts',
     // TODO: Junit file somehow still not being generated
-    reporters: ['default', 'junit'],
+    reporters: ['junit'],
+    // as Copilot explained:
+    // > The Angular builder’s outputFile (string) option overrides Vitest’s own outputFile.
+    // > You cannot use multiple Vitest reporters or custom reporter config
+    // > via vitest.config.ts when running tests through ng test.
+    // maybe that would work with separate ng tasks for junit and HTML reporter?
+    outputFile: {
+      html: './vitest/html-report.html',
+      junit: './vitest-junit-report.xml',
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 });
