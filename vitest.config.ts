f8d1a908c15a7baa26b7e47b5a10ab3217b233f8
Witek <wiiitek>
2026-02-13 22:51:12 +0100

    Clean-up

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,16 +8,16 @@ export default defineConfig({
     globals: true,
     setupFiles: './src/test-setup.ts',
     // TODO: Junit file somehow still not being generated
-    reporters: ['junit'],
+    // reporters: ['junit'],
     // as Copilot explained:
     // > The Angular builder’s outputFile (string) option overrides Vitest’s own outputFile.
     // > You cannot use multiple Vitest reporters or custom reporter config
     // > via vitest.config.ts when running tests through ng test.
     // maybe that would work with separate ng tasks for junit and HTML reporter?
-    outputFile: {
-      html: './vitest/html-report.html',
-      junit: './vitest-junit-report.xml',
-    },
+    // outputFile: {
+    //   html: './vitest/html-report.html',
+    //   junit: './vitest-junit-report.xml',
+    // },
     coverage: {
       provider: 'v8',
       reporter: ['lcov'],
